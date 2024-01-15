53be79ebd23451a94555731abb4c2226cb2038bd
Rodrigo Maia <rodrigomb@gmail.com>
2024-01-15 08:08:40 -0300

    list s3 files

--- dist/index.js
+++ dist/index.js
@@ -2,10 +2,33 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = void 0;
 // src/index.ts
-const handler = async (event) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Hello from Lambda!" }),
-    };
+const aws_sdk_1 = require("aws-sdk");
+const handler = async (_event, _context) => {
+    var _a;
+    // Configurar as credenciais e a região do S3
+    const s3 = new aws_sdk_1.S3({
+        region: "us-east-1", // Substitua pela região do seu bucket
+        accessKeyId: "sua-chave-de-acesso", // Substitua com suas credenciais
+        secretAccessKey: "sua-chave-secreta", // Substitua com suas credenciais
+    });
+    // Nome do bucket do S3
+    const bucketName = "teste-lambda-ts";
+    try {
+        // Listar objetos no bucket
+        const data = await s3.listObjectsV2({ Bucket: bucketName }).promise();
+        // Extrair nomes dos arquivos
+        const fileNames = ((_a = data.Contents) === null || _a === void 0 ? void 0 : _a.map((obj) => obj.Key)) || [];
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ files: fileNames }),
+        };
+    }
+    catch (error) {
+        console.error("Error listing S3 objects:", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Internal server error" }),
+        };
+    }
 };
 exports.handler = handler;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,36 @@
 // src/index.ts
-export const handler = async (event: any): Promise<any> => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Lambda!" }),
-  };
+import { S3 } from "aws-sdk";
+import { Handler } from "aws-lambda";
+
+export const handler: Handler = async (_event: any, _context: any) => {
+  // Configurar as credenciais e a região do S3
+  const s3 = new S3({
+    region: "us-east-1", // Substitua pela região do seu bucket
+    accessKeyId: "sua-chave-de-acesso", // Substitua com suas credenciais
+    secretAccessKey: "sua-chave-secreta", // Substitua com suas credenciais
+  });
+
+  // DANDO Timeout, ACREDITO QUE SEJA PERMISSÃO
+
+  // Nome do bucket do S3
+  const bucketName = "teste-lambda-ts";
+
+  try {
+    // Listar objetos no bucket
+    const data = await s3.listObjectsV2({ Bucket: bucketName }).promise();
+
+    // Extrair nomes dos arquivos
+    const fileNames = data.Contents?.map((obj) => obj.Key) || [];
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ files: fileNames }),
+    };
+  } catch (error) {
+    console.error("Error listing S3 objects:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal server error" }),
+    };
+  }
 };
